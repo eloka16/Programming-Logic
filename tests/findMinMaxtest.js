822d672956f325fca0e1c6c3fbf7f2dc95d378ab
frank chukwu <frank chukwu>
2017-01-07 10:37:50 +0100

    make findMinMax test cases up to 10

--- tests/findMinMaxtest.js
+++ tests/findMinMaxtest.js
@@ -1,5 +1,6 @@
-(function() {
   'use strict';
+
+  var findMinMax = require('../labs.js').findMinMax;
 
   describe('Min-Max Numbers in a List: ', function () {
 
@@ -16,6 +17,15 @@
       it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
         expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).toEqual([2, 78]);
       });
+      it('should return [3, 10] for [3, 10, 4]', function () {
+        expect(findMinMax([3, 10, 4])).toEqual([3, 10]);
+      })
+      it('should return [-2, 8] for [-2, 4, 5, 1, 8]', function() {
+        expect(findMinMax([-2, 4, 5, 1, 8])).toEqual([-2, 8]);
+      })
+      it('should return [0, 18] for [3, 0, 1, 5, 18]', function () {
+        expect(findMinMax([3, 0, 1, 5, 18])).toEqual([0, 18]);
+      })
 
     });
 
@@ -24,9 +34,16 @@
       it('should return [4] for [4, 4, 4, 4]', function () {
         expect(findMinMax([4, 4, 4, 4])).toEqual([4]);
       });
+      it('should return [-2] for [-2, -2, -2]', function () {
+        expect(findMinMax([-2, -2, -2])).toEqual([-2]);
+      })
+      it('should return [5] for [5, 5]', function () {
+        expect(findMinMax([5, 5])).toEqual([5]);
+      })
+      it('should return [8] for [8, 8, 8, 8]', function () {
+        expect(findMinMax([8, 8, 8, 8])).toEqual([8]);
+      })
 
     });
 
   });
-
-})();
