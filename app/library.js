184a38f7948731c3b5c23776fde7c62f950e0288
frank chukwu <frank chukwu>
2017-01-07 11:15:21 +0100

    write function to pass fizzbuzz test

--- app/library.js
+++ app/library.js
@@ -13,8 +13,15 @@ module.exports = {
     },
 
     fizzBuzz: function (n) {
-        // TODO
-	return '';
+        if (n % 3 === 0 && n % 5 === 0){
+            return "FizzBuzz";
+        }else if (n % 3 === 0) {
+            return "Fizz";
+        } else if (n % 5 === 0){
+            return "Buzz";
+        } else {
+            return n;
+        }
     },
 
     aritGeo: function (arr) {
